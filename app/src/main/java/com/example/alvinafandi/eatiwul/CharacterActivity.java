8d17fcc5b53ad2a1601def02f9dec08716564b64
YuukiRen <reinaldoalvin1@gmail.com>
2018-09-09 21:52:02 +0700

    + button

--- app/src/main/java/com/example/alvinafandi/eatiwul/CharacterActivity.java
+++ app/src/main/java/com/example/alvinafandi/eatiwul/CharacterActivity.java
@@ -9,22 +9,35 @@ import android.widget.Button;
 
 public class CharacterActivity extends AppCompatActivity implements View.OnClickListener {
     private Button select;
-    ViewPager viewPager;
+    private ViewPager viewPager;
+    private Button slideRight;
+    private Button slideLeft;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character);
         select = (Button) findViewById(R.id.selectChar);
         viewPager = (ViewPager) findViewById(R.id.viewPager);
+        slideLeft = (Button) findViewById(R.id.slideLeft);
+        slideRight = (Button) findViewById(R.id.slideRight);
 
+        slideRight.setOnClickListener(this);
+        slideLeft.setOnClickListener(this);
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
         select.setOnClickListener(this);
+
 
         viewPager.setAdapter(viewPagerAdapter);
     }
     public void onClick(View view){
         if (view == select){
             homePage();
+        }
+        if (view == slideLeft){
+            viewPager.setCurrentItem(viewPager.getCurrentItem()+1,true);
+        }
+        if (view == slideRight){
+            viewPager.setCurrentItem(viewPager.getCurrentItem()-1,true);
         }
     }
     private void homePage(){
